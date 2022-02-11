ab62abcabdb646b07135b06269845528a5d3087b
Pranay Gaikwad <pranay_gaikwad@persistent.com>
2022-02-11 16:17:00 +0530

    Added fn header

--- math.py
+++ math.py
@@ -9,10 +9,4 @@ def multiply(x,y):
     return x*y #on bug456 branch
 # DIVIDE IMPLEMENTATION
 def divide(x,y):
-    if Y==0:
-        return DIVIDE_BY_0_ERROR
-    else:
-        return x/y  #on master branch
-# SQUARE IMPLEMENTATION
-def square(x):
-    pass
+   pass
