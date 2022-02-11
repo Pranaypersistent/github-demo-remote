0861a596cf1000782043e45213a1cc86c90e3d56
Pranaypersistent <99319458+Pranaypersistent@users.noreply.github.com>
2022-02-11 16:12:26 +0530

    Initial Commit of math.py

---
+++ math.py
@@ -0,0 +1,18 @@
+# ADD IMPLEMENTATION
+def add(x,y):
+    return x+y
+# SUBTRACT IMPLEMENTATION
+def subtract(x,y):
+    return x-y # on master branch
+# MULTIPLY IMPLEMENTATION
+def multiply(x,y):
+    return x*y #on bug456 branch
+# DIVIDE IMPLEMENTATION
+def divide(x,y):
+    if Y==0:
+        return DIVIDE_BY_0_ERROR
+    else:
+        return x/y  #on master branch
+# SQUARE IMPLEMENTATION
+def square(x):
+    pass
